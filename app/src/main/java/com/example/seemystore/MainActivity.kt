14e06ab0a8dc84980f496025a4ab25321cba8707
jheske <jeheske@gmail.com>
2018-07-26 10:02:15 -0400

    Fix directory names

--- app/src/main/java/com/example/seemystore/MainActivity.kt
+++ app/src/main/java/com/example/seemystore/MainActivity.kt
@@ -9,41 +9,8 @@ import com.example.seemystore.api.ApiInterface
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
 
-    //https://antonioleiva.com/retrofit-android-kotlin/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val apiService = ApiClient.client.create(ApiInterface::class.java)
-        val call = apiService.getStoresList()
     }
-
-
-/*
-    fun addSampleData() {
-        StoreTable(this)
-                .dbInsertStore(Store("Store1","1","Store1",
-                        "Main Street","Atlanta","GA","30096",
-                        "770-555-5555",28.078052F, -82.583301F))
-        StoreTable(this)
-                .dbInsertStore(Store("Store2","2","Store2",
-                        "Main Street","Atlanta","GA","30096",
-                        "770-555-5555",28.078052F, -82.583301F))
-        StoreTable(this)
-                .dbInsertStore(Store("Store3","3","Store3",
-                        "Main Street","Atlanta","GA","30096",
-                        "770-555-5555",28.078052F, -82.583301F))
-        StoreTable(this)
-                .dbInsertStore(Store("Store4","4","Store4",
-                        "Main Street","Atlanta","GA","30096",
-                        "770-555-5555",28.078052F, -82.583301F))
-        StoreTable(this)
-                .dbInsertStore(Store("Store5","5","Store5",
-                        "Main Street","Atlanta","GA","30096",
-                        "770-555-5555",28.078052F, -82.583301F))
-        StoreTable(this)
-                .dbInsertStore(Store("Store6","6","Store6",
-                        "Main Street","Atlanta","GA","30096",
-                        "770-555-5555",28.078052F, -82.583301F))
-    }*/
 }
